83316a63ac215aa7bfa92554e1a0f54ae2808330
Maja Mitreska <48536512+MajaMitreska@users.noreply.github.com>
2022-03-24 11:59:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from xAMR import xAMR
 
 if __name__ == '__main__':
-    x_sentence = xAMR('x', '<sentence in x language.>')
+    x_sentence = xAMR('x_lang', '<sentence in x language.>')
     x_sentence.pipeline()
 
     print(f'AMR graph representation: {x_sentence.amr_graph}')
-    print(f'Final mk sentence: {x_sentence.translated_en_to_x_language}')
+    print(f'Final {x_lang} sentence: {x_sentence.translated_en_to_x_language}')
